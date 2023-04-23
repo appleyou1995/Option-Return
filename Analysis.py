ba3cccf29025505bc13b4789e1767fc7821b814e
appleyou1995 <jouping.yu@gmail.com>
2023-04-23 17:19:41 +0800

    Version: 20230129

--- Analysis.py
+++ Analysis.py
@@ -1,88 +1,127 @@
 import pandas   as pd
 import numpy    as np
+
 from pathlib import Path
+from pandas.tseries.offsets import DateOffset
 
 
-# %%  X variables from Lillian (volatility_var.py)
-
-
-folder = Path('D:/Google/我的雲端硬碟/學術研究/論文著作/Option Return/Data/陳韻安/volatility_var')
-
-# Stock Data
-stock_data   = pd.read_pickle(folder/'stock_data.pkl')
-# stock_data_d = pd.read_pickle(folder/'stock_data_d.pkl')
-# option_data  = pd.read_pickle(folder/'option_data.pkl')
-
-# Implied Volatility
-implied_call = pd.read_pickle(folder/'implied_call.pkl')
-implied_put  = pd.read_pickle(folder/'implied_put.pkl')
-implied_vol  = pd.read_pickle(folder/'implied_vol.pkl')
-
-# Realized Volatility
-realized_vol = pd.read_pickle(folder/'realized_vol.pkl')
-
-# Volatility Var
-vol_var      = pd.read_pickle(folder/'vol_var.pkl')
-
-
-# %%  Y variable from SAS (Option Return - 20221006.egp)
+# %%  Option Data from SAS (Option Return - 20221006.egp)
 
 
 folder_SAS = Path('D:/Google/我的雲端硬碟/學術研究/論文著作/Option Return/Data/SAS - Output')
-option_data_all = pd.read_csv(folder_SAS/"Y.csv")
+option_data_all = pd.read_csv(folder_SAS/"Y_SHROUT.csv")
 
-# for col in option_data_all.columns:
-#     print(col)
+for col in option_data_all.columns: print(col)
+
+option_data_Original = option_data_all[['date', 'cusip', "SHROUT", 'Buy & hold until month-end (%)']]
+option_data_Original.rename(columns = {'Buy & hold until month-end (%)':'Option_Return'}, inplace = True)
+option_data_Original.rename(columns = {'SHROUT':'Option_Size'}, inplace = True)
+
+
+option_data_Original['date'] = pd.to_datetime(option_data_Original['date'])
+option_data_Original.insert(loc = 1, column = 'Next_Month_Option', value = option_data_Original['date'] + DateOffset(months=1))
+option_data_Original = option_data_Original.sort_values(by=['date', 'cusip'])
+
+option_data_Original = option_data_Original.set_index('date', drop = False)
+option_data_Original.rename(columns = {'date':'This_Month_Option'}, inplace = True)
+
+
+# groupby_date
+option_data_groupby_date = pd.DataFrame(option_data_Original.groupby('date'))
+
+
+# %%  SKEW Data from Sherry
+
+
+folder_Data = Path('D:/Google/我的雲端硬碟/學術研究/論文著作/Option Return/Data')
+SKEW_Sherry = pd.read_csv(folder_Data/"skew12_monthly.csv")
+
+for col in SKEW_Sherry.columns: print(col)
+
+SKEW_Sherry.rename(columns = {'size':'Stock_Size'}, inplace = True)
+
+SKEW_Sherry['date'] = pd.to_datetime(SKEW_Sherry['date'])
+SKEW_Sherry.insert(loc = 1, column = 'Next_Month_Stock', value = SKEW_Sherry['date'] + DateOffset(months=1))
+SKEW_Sherry = SKEW_Sherry.sort_values(by=['date', 'cusip'])
+
+SKEW_Sherry = SKEW_Sherry.set_index('date', drop = False)
+SKEW_Sherry.rename(columns = {'date':'This_Month_Stock'}, inplace = True)
+
+
+# groupby_date
+SKEW_groupby_date = pd.DataFrame(SKEW_Sherry.groupby('date'))
+
+
+# %%  Fama-French five-factor model
+
+FF5_data_all = pd.read_csv(folder_Data/'F-F_monthly.csv')
+
+FF5_data = FF5_data_all.iloc[390:702,2:7]
+FF5_data.index = SKEW_groupby_date[0]
+
+
+# %%  Combine Above Dataframes
+
+
+option_SKEW_0 = option_data_Original.merge(SKEW_Sherry, left_on = ['This_Month_Option', 'cusip'], right_on = ['This_Month_Stock', 'cusip'])
+option_SKEW_0_count = pd.DataFrame(option_SKEW_0.groupby('This_Month_Option')['cusip'].count()).sort_values(by=['cusip'])
+option_SKEW_0_count.rename(columns = {'cusip':'count'}, inplace = True)
+
+# option_SKEW_0.to_csv('D:/Google/我的雲端硬碟/學術研究/論文著作/Option Return/Data/option_SKEW_0.csv')
+# option_SKEW_0_count.to_csv('D:/Google/我的雲端硬碟/學術研究/論文著作/Option Return/Data/option_SKEW_0_count.csv')
+
+
+# option_SKEW_1 = option_data_Original.merge(SKEW_Sherry, left_on = ['This_Month_Option', 'cusip'], right_on = ['Next_Month_Stock', 'cusip'])
+# option_SKEW_1_count = pd.DataFrame(option_SKEW_1.groupby('This_Month_Option')['cusip'].count()).sort_values(by=['cusip'])
+# option_SKEW_1_count.rename(columns = {'cusip':'count'}, inplace = True)
+
+
+# option_SKEW_2 = option_data_Original.merge(SKEW_Sherry, left_on = ['Next_Month_Option', 'cusip'], right_on = ['This_Month_Stock', 'cusip'])
+# option_SKEW_2_count = pd.DataFrame(option_SKEW_2.groupby('This_Month_Option')['cusip'].count()).sort_values(by=['cusip'])
+# option_SKEW_2_count.rename(columns = {'cusip':'count'}, inplace = True)
+
+
+# option_SKEW = pd.merge(option_SKEW, FF5_data, left_on = ['This_Month_Stock'], right_index = True)
+
+
+
+######################################################################################################################
+                                                                                                                     #
+option_data = pd.merge(option_SKEW_0, option_SKEW_0_count, left_on = ['This_Month_Option'], right_index = True)      #
+                                                                                                                     #
+######################################################################################################################
+
+
+option_data = option_data[['This_Month_Option', 'cusip', "skew1", "skew2", "Stock_Size", "Option_Size", 'Option_Return', 'count']]
+option_data = option_data.set_index('This_Month_Option')
+option_data.index.name = 'date'
+
+
+# %%  Option Data with SKEW (Option Return - 20230129.egp)
+
+# folder = Path('D:/Google/我的雲端硬碟/學術研究/論文著作/Option Return/Data/SAS - Output')
+
+# # Option Data with SKEW
+# Option_Skew   = pd.read_csv(folder/'Option_SKEW.csv')
+
+# for col in Option_Skew.columns: print(col)
     
-option_data     = option_data_all[['date', 'PERMNO', 'Buy & hold until month-end (%)']]
-option_data.rename(columns = {'Buy & hold until month-end (%)':'Option_Return'}, inplace = True)
+# option_data = Option_Skew[['date', 'cusip', "skew1", "skew2", "size", "SHROUT", 'Buy & hold until month-end (%)']]
+
+# option_data.rename(columns = {'Buy & hold until month-end (%)':'Option_Return'}, inplace = True)
+# option_data.rename(columns = {'size':'Stock_Size'}, inplace = True)
+# option_data.rename(columns = {'SHROUT':'Option_Size'}, inplace = True)
+
+# option_data['date'] = pd.to_datetime(option_data['date'])
+# option_data = option_data.set_index('date')
+# option_data_df = pd.DataFrame(option_data.groupby('date'))
 
 
 # %%  from Lillian (volatility analysis.py)
 
-realized_vol = realized_vol.shift(1).iloc[1:,:]
-vol_df = pd.DataFrame()
-for i in range(len(realized_vol)):
-    data = realized_vol.iloc[[i],:].copy()
-    temp = data.T.reset_index()
-    temp.columns = ['PERMNO', 'realized_vol']
-    vol_df = pd.concat([vol_df, temp], axis=0)
 
-implied_call = implied_call.shift(1).iloc[1:,:]
-temp1_df = pd.DataFrame()
-for i in range(len(implied_call)):
-    data = implied_call.iloc[[i],:].copy()
-    temp = data.T.reset_index()
-    temp.columns = ['PERMNO', 'implied_call']
-    temp1_df = pd.concat([temp1_df, temp], axis=0)   
-   
-implied_put = implied_put.shift(1).iloc[1:,:]
-temp2_df = pd.DataFrame()
-for i in range(len(implied_put)):
-    data = implied_put.iloc[[i],:].copy()
-    temp = data.T.reset_index()
-    temp.columns = ['PERMNO', 'implied_put']
-    temp2_df = pd.concat([temp2_df, temp], axis=0)   
+# Portfolio Sorted
 
-
-folder_pkl = Path('D:/Google/我的雲端硬碟/學術研究/論文著作/Option Return/Data/陳韻安/Lillian')
-
-RET = pd.read_pickle(folder_pkl/'data_balanced.pkl').iloc[:,:2]
-vol_fin_df = pd.concat([vol_df,temp1_df['implied_call'],temp2_df['implied_put']], axis=1)
-vol_fin_df.index = RET.index
-vol_fin_df = pd.concat([RET,vol_fin_df.iloc[:,1:]], axis=1)
-
-vol_temp = vol_fin_df.dropna()
-
-# Volatility spreads
-vol_temp['rivolspread'] = vol_temp['realized_vol'] - (vol_temp['implied_call'] + vol_temp['implied_put'])*0.5
-vol_temp['ivolspread'] = vol_temp['implied_call'] - vol_temp['implied_put']
-
-
-#%%  from Lillian (volatility analysis.py)
-
-
-# Portfolio Sorted on Realized and Implied Volatilities
 def portfolio(data, sorted_var, split_num):
     n = int(len(data)/split_num)
     data_sorted = data.sort_values(sorted_var)
@@ -98,141 +137,126 @@ def portfolio(data, sorted_var, split_num):
     return output
 
 
-vol_temp_df = pd.DataFrame(vol_temp.groupby('date'))
-vol_temp_df_group = pd.DataFrame(vol_temp.groupby('date')['PERMNO'].count()).sort_values(by=['PERMNO'])
+# %%  【Table】 Average returns of portfolios  (2022_RFS_Option Return Predictability - Table 3)
 
-for col in vol_temp.columns: print(col)
-
-
-# Customize 1
-sorted_var = 'realized_vol'
 split_num  = 5
+
+option_data = option_data[option_data['count'] >= split_num]
+option_data_df = pd.DataFrame(option_data.groupby('date'))
+
+
+######  SKEW 1  &  Eqaul-Weighted  ######
+
+sorted_var = 'skew1'
+portfolio_SKEW_1_EW = pd.DataFrame()
 
 portfolio_fin = pd.DataFrame()
 for i in range(split_num):
     portfolio_df = pd.DataFrame()
-    for j in range(len(vol_temp_df)):
-        data         = vol_temp_df.iloc[j,1].copy()
+    for j in range(len(option_data_df)):
+        data         = option_data_df.iloc[j,1].copy()
         output       = portfolio(data, sorted_var, split_num)
-        portfolio_df = pd.concat([portfolio_df, pd.DataFrame(output[i].mean()).T.iloc[:,1:]])
-    portfolio_df.index = realized_vol.index
+        portfolio_df = pd.concat([portfolio_df, pd.DataFrame(output[i].mean()).T])
+    portfolio_df.index = option_data_df[0]
     portfolio_fin = pd.concat([portfolio_fin, pd.DataFrame(portfolio_df.mean()).T])
-portfolio_fin.index = list(range(1, 1 + split_num))
+
+portfolio_SKEW_1_EW['SKEW_1_EW'] = portfolio_fin['Option_Return']
+portfolio_SKEW_1_EW.index = list(range(1, 1 + split_num))
 
 
-# Customize 2
-sorted_var = 'ivolspread'
-split_num  = 5
 
-portfolio_ivolspread = pd.DataFrame()
+######  SKEW 2  &  Eqaul-Weighted  ######
+
+sorted_var = 'skew2'
+portfolio_SKEW_2_EW = pd.DataFrame()
+
+portfolio_fin = pd.DataFrame()
 for i in range(split_num):
     portfolio_df = pd.DataFrame()
-    for j in range(len(vol_temp_df)):
-        data         = vol_temp_df.iloc[j,1].copy()
+    for j in range(len(option_data_df)):
+        data         = option_data_df.iloc[j,1].copy()
         output       = portfolio(data, sorted_var, split_num)
-        portfolio_df = pd.concat([portfolio_df, pd.DataFrame(output[i].mean()).T.iloc[:,1:]])
-    portfolio_ivolspread = pd.concat([portfolio_ivolspread, pd.DataFrame(portfolio_df.mean()).T])
-portfolio_ivolspread.index = list(range(1, 1 + split_num))
+        portfolio_df = pd.concat([portfolio_df, pd.DataFrame(output[i].mean()).T])
+    portfolio_df.index = option_data_df[0]
+    portfolio_fin = pd.concat([portfolio_fin, pd.DataFrame(portfolio_df.mean()).T])
+
+portfolio_SKEW_2_EW['SKEW_2_EW'] = portfolio_fin['Option_Return']
+portfolio_SKEW_2_EW.index = list(range(1, 1 + split_num))
 
 
-#%%  Fama-French three-factor model
 
+######  SKEW 1  ######
 
-ff3_data = pd.read_csv(folder_pkl/'F-F_Research_Data_Factors.csv',
-                       skiprows=3, index_col=0, nrows=1134)/100
-# ff3_data = ff3_data.iloc[-300:,:]  # Lillian Original Version
-ff3_data = ff3_data.iloc[-299:,:]    # Fix to Clear Error
-ff3_data.index = realized_vol.index
+sorted_var = 'skew1'
+portfolio_SKEW_1 = pd.DataFrame()
 
-
-#%%      
-
-
-import statsmodels.formula.api as smf
-
-sorted_var = 'rivolspread'
-split_num = 5
-
-ff3_alpha = pd.DataFrame()
+EW_VW = pd.DataFrame()
+portfolio_fin = pd.DataFrame()
 for i in range(split_num):
-    data_df = pd.DataFrame()
-    for j in range(len(vol_temp_df)):
-        data = vol_temp_df.iloc[j,1].iloc[:,1:].copy()
-        data['Mkt_RF'] = ff3_data['Mkt-RF'][j]
-        data['SMB'] = ff3_data['SMB'][j]
-        data['HML'] = ff3_data['HML'][j]
-        data['RF'] = ff3_data['RF'][j]
-        data['y'] = data['RET']-data['RF']
-        data = data.astype('float64')
-    
-        output = portfolio(data, sorted_var, split_num)
-        data_df = pd.concat([data_df, output[i]])
+    portfolio_df = pd.DataFrame()
+    Value_Weighted = pd.DataFrame(columns=['VW'], index=range(len(option_data_df)))
+    for j in range(len(option_data_df)):
+        data         = option_data_df.iloc[j,1].copy()
+        output       = portfolio(data, sorted_var, split_num)
+        portfolio_df = pd.concat([portfolio_df, pd.DataFrame(output[i].mean()).T])
+        Value_Weighted.iloc[j] = np.average(output[i]['Option_Return'], weights=output[i]['Option_Size'])
+    portfolio_df.index = option_data_df[0]
+    Value_Weighted.index = option_data_df[0]
+    EW_VW = pd.concat([portfolio_df, Value_Weighted], axis=1)
+    portfolio_fin = pd.concat([portfolio_fin, pd.DataFrame(EW_VW.mean()).T])
 
-    reg = smf.ols('y ~ Mkt_RF + SMB + HML', data = data_df).fit(cov_type='HAC', cov_kwds={'maxlags':1})
-    ff3_alpha = pd.concat([ff3_alpha, pd.DataFrame(reg.params).T])
-ff3_alpha.index = list(range(1, 1 + split_num))
-
-i = 0
-portfolio_1 = pd.DataFrame()
-for j in range(len(vol_temp_df)):
-    data = vol_temp_df.iloc[j,1].iloc[:,1:].copy()
-    data['Mkt_RF'] = ff3_data['Mkt-RF'][j]
-    data['SMB'] = ff3_data['SMB'][j]
-    data['HML'] = ff3_data['HML'][j]
-    data['RF'] = ff3_data['RF'][j]
-    data['y'] = data['RET']-data['RF']
-    data = data.astype('float64')
-    
-    output = portfolio(data, sorted_var, split_num)
-    portfolio_1 = pd.concat([portfolio_1, output[i]])
-
-i = 4
-portfolio_5 = pd.DataFrame()
-for j in range(len(vol_temp_df)):
-    data = vol_temp_df.iloc[j,1].iloc[:,1:].copy()
-    data['Mkt_RF'] = ff3_data['Mkt-RF'][j]
-    data['SMB'] = ff3_data['SMB'][j]
-    data['HML'] = ff3_data['HML'][j]
-    data['RF'] = ff3_data['RF'][j]
-    data['y'] = data['RET']-data['RF']
-    data = data.astype('float64')
-    
-    output = portfolio(data, sorted_var, split_num)
-    portfolio_5 = pd.concat([portfolio_5, output[i]])
-
-port_1_alpha = pd.DataFrame()
-for i in range(len(vol_temp_df)):
-    time = vol_temp_df[0][i]
-    data = portfolio_1.loc[time,:].copy()
-    reg = smf.ols('y ~ Mkt_RF + SMB + HML', data = data).fit(cov_type='HAC', cov_kwds={'maxlags':1})
-    port_1_alpha.loc[i, 'alpha'] = reg.params[0]
-    
-port_5_alpha = pd.DataFrame()
-for i in range(len(vol_temp_df)):
-    time = vol_temp_df[0][i]
-    data = portfolio_5.loc[time,:].copy()
-    reg = smf.ols('y ~ Mkt_RF + SMB + HML', data = data).fit(cov_type='HAC', cov_kwds={'maxlags':1})
-    port_5_alpha.loc[i, 'alpha'] = reg.params[0]
+portfolio_SKEW_1['SKEW_1_EW'] = portfolio_fin['Option_Return']
+portfolio_SKEW_1['SKEW_1_VW'] = portfolio_fin['VW']
+portfolio_SKEW_1.index = list(range(1, 1 + split_num))
 
 
-import scipy.stats as stats
 
-stats.ttest_ind(port_5_alpha, port_1_alpha)
-stats.ttest_ind(portfolio_5['RET'], portfolio_1['RET'])
+######  SKEW 2  ######
+
+sorted_var = 'skew2'
+portfolio_SKEW_2 = pd.DataFrame()
+
+EW_VW = pd.DataFrame()
+portfolio_fin = pd.DataFrame()
+for i in range(split_num):
+    portfolio_df = pd.DataFrame()
+    Value_Weighted = pd.DataFrame(columns=['VW'], index=range(len(option_data_df)))
+    for j in range(len(option_data_df)):
+        data         = option_data_df.iloc[j,1].copy()
+        output       = portfolio(data, sorted_var, split_num)
+        portfolio_df = pd.concat([portfolio_df, pd.DataFrame(output[i].mean()).T])
+        Value_Weighted.iloc[j] = np.average(output[i]['Option_Return'], weights=output[i]['Option_Size'])
+    portfolio_df.index = option_data_df[0]
+    Value_Weighted.index = option_data_df[0]
+    EW_VW = pd.concat([portfolio_df, Value_Weighted], axis=1)
+    portfolio_fin = pd.concat([portfolio_fin, pd.DataFrame(EW_VW.mean()).T])
+
+portfolio_SKEW_2['SKEW_2_EW'] = portfolio_fin['Option_Return']
+portfolio_SKEW_2['SKEW_2_VW'] = portfolio_fin['VW']
+portfolio_SKEW_2.index = list(range(1, 1 + split_num))
 
 
-#%%
+
+######  Merge  ######
 
 
-# Fama-MacBeth Regression
-from linearmodels import FamaMacBeth
+Table_1 = pd.concat([portfolio_SKEW_1, portfolio_SKEW_2], axis=1).T
 
-temp = vol_temp.copy().reset_index()
-temp = temp.set_index(['PERMNO', 'date'])
-temp = temp.astype('float64')
 
-fm = FamaMacBeth.from_formula('RET ~ 1 + rivolspread', data=temp)
-res = fm.fit(cov_type='kernel', debiased=False, bandwidth=4)
-print(res.summary)
-res.params
-res.tstats
+
+
+# %%  【Table】 Portfolios Sorted on Volatility Features  (Lillian - Table 4)
+
+
+
+
+
+
+
+# %%  【Table】 Fama-MacBeth Regressions  (Lillian - Table 5)
+
+
+
+
+
+
